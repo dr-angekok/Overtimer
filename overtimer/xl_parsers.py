6f08d8c8f9cba957cdac86d95288b1e61f2913ac
Angekok <angekok@mail.ru>
2022-01-17 21:51:08 +0300

    Add order parser and tests.

--- overtimer/xl_parsers.py
+++ overtimer/xl_parsers.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import datetime
+
 
 def personal_parser(filename):
     data = pd.ExcelFile(filename).parse(sheet_name=0, skiprows=13)
@@ -16,3 +18,24 @@ def personal_parser(filename):
     data['hours'] = data['hours'].apply(int)
     data['numbers'] = data['numbers'].apply(int)
     return data
+
+
+def to_zero_date(item):
+    if not isinstance(item, datetime.datetime):
+        return datetime.date(1900, 1, 1)
+    return item
+
+
+def get_order_number(filename):
+    sheets = pd.ExcelFile(filename)
+    frames = []
+    for sheet_name in sheets.sheet_names:
+        sheet = sheets.parse(sheet_name)
+        sheet.dropna(inplace=True)
+        if sheet.shape[0] and sheet.shape[1] == 3:
+            sheet.drop(sheet.columns[[0]], axis=1, inplace=True)
+            sheet.columns = ['numbers', 'dates'] if '/' in str(sheet[sheet.columns[[0]]].values[0]) else ['dates', 'numbers']
+            frames.append(sheet)
+    data = pd.concat(frames)
+    data['dates'] = data['dates'].apply(to_zero_date)
+    return data.sort_values(by='dates').iloc[-1:]['numbers'].values[0]
--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -1,6 +1,7 @@
 from overtimer import xl_parsers
 
 PERSONAL_FILENAME = 'tests/fixtures/persons.xls'
+ORDER_FILENAME = 'tests/fixtures/orders.xlsx'
 
 
 def test_personal_parser():
@@ -11,3 +12,8 @@ def test_personal_parser():
     assert 'Бидон Надоев' in test_data['names'].values
     assert 28448 in test_data['numbers'].values
     assert 2 in test_data['hours'].values
+
+
+def test_get_order_number():
+    number = xl_parsers.get_order_number(ORDER_FILENAME)
+    assert number == '278/ка'
